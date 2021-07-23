694a169335b921badb8929838a02e9acd44ee308
Zalozhnyy <niczz9797@gmail.com>
2021-07-23 16:30:09 +0300

    heroku

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    '0.0.0.0',
+    'localhost'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
@@ -82,20 +85,28 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'chatapp',
+#         'USER': 'postgres',
+#         'PASSWORD': '753159',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
 #     }
 # }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chatapp',
-        'USER': 'postgres',
-        'PASSWORD': '753159',
-        'HOST': '127.0.0.1',
+        'NAME': 'dda1d9a0q312sn',
+        'USER': 'xxwtjmntefcrmm',
+        'PASSWORD': '4ec25cc037892b87aaddbb3db270bb0b4539f019e53cccf59d005d1a220083a1',
+        'HOST': 'ec2-52-208-221-89.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
